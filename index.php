6f0ad801f1e8242ff5a1481b58a0c30ef548236d
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2022-09-15 11:16:28 -0400

    ajouter style dans style.css

--- index.php
+++ index.php
@@ -20,6 +20,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+        h1{
+            background-color: blue;
+            color: gray;
+        }
+    </style>
+
     <title>Document</title>
 </head>
 <body>
