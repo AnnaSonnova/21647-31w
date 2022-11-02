6e3c58a6a1d265896c27f2940e73528e7d812cb3
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2022-11-01 21:59:16 -0400

    c15-ajoute 2 champs personnaliser: duree de cours et professeur dans la structure de category-cours

--- category-cours.php
+++ category-cours.php
@@ -1,1 +1,34 @@
-<h2>Duree du cours<?php the_field('heading'); ?></h2>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscore
+ */
+?>
+<!-- h1 class="trace">front-page.php</h1 -->
+<?php get_header(); ?>
+
+    <main class="site__main">
+    <?php
+		if ( have_posts() ) :
+            while ( have_posts() ) :
+				the_post(); ?>
+                <h1><a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?></a></h1>
+                <h4>Durée du cours: <?php the_field('duree'); ?></h4>
+                <h4>Professeur: <?php the_field('professeur'); ?></h4>
+                <?php the_content(null, true); ?>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
+  
+    </main>    
+<?php get_footer(); ?>
+</html>
