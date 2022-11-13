45b180853f2d4c289cfcddbc8ea3c73500d3abae
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2022-11-13 16:06:20 -0500

    c17 modification d'apperence de vue cours (wp_trim_words de content et substr de titre).

--- category-cours.php
+++ category-cours.php
@@ -22,19 +22,24 @@
             while ( have_posts() ) :
 				the_post(); ?>
                 <article class="liste__cours">
-                <h1><a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?></a></h1>
+                <h1><a href="<?php the_permalink();
+                ?>">
+                <?php $titre = the_title('','',false);  echo substr($titre,8,-6)?></a></h1>
                 <ul class="category__champ">
                     <li>Durée du cours: <?php the_field('duree'); ?></li>
                     <li>Professeur: <?php the_field('professeur'); ?></li>
                     <li>Préalable: <?php the_field('prealable'); ?></li>
                 </ul>
-                <?php the_content($more_link_text, true); 
+                <p>
+
+                <?php
+                //  the_content($more_link_text, true); 
                 
-                wp_trim_words($more_link_text,3,"...");
+                echo wp_trim_words(get_the_excerpt(),30,"...");
                  
-                //wp_trim_words()
+                
                 ?>
+                </p>
                 <article>
             <?php endwhile; ?>
         <?php endif; ?>
