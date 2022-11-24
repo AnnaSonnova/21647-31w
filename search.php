08e8e79944f083f3b7907fcd873e005ce9f6fc13
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2022-11-24 11:52:47 -0500

    ef-ajouter style pour resultat de recherche

--- search.php
+++ search.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * Template pour les resultats de recherche
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -23,17 +18,14 @@
         "container_class"=>"menu__evenement"
         
     ));?>
-<section class="liste__accueil">
+<section class="liste__recherche">
 
     <?php if ( have_posts() ) :
         while ( have_posts() ) :
             the_post(); ?>
            
-            <article class="liste__accueil__cours">
+            <article class="liste__recherche__cours">
 
-                
-               
-                
                 <?php
                 $montableau = get_the_category();
                 $boolGalerie = false;
